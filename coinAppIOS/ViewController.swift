fc6328ff0f1796743fdeb13b53927d5b4176da2e
walid0010 <walidchekir98@gmail.com>
2021-07-28 21:37:58 +0100

    Initial Commit

---
+++ coinAppIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coinAppIOS
+//
+//  Created by chekir walid on 28/7/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
